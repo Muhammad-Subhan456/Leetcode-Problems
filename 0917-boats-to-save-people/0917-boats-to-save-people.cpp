8f95dc17d18dffde9222a17b0d5b1c9b170fd169
Muhammad Subhan <sbhnamir456@gmail.com>
2024-08-19 03:35:17 -0700

    Time: 60 ms (41.89%), Space: 45.8 MB (10.39%) - LeetHub

---
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int numRescueBoats(vector<int>& people, int limit) {
+        int lo=0,hi=people.size()-1,count=0;
+        sort(people.begin(),people.end());
+        while(lo<=hi){
+            if(people[lo]+people[hi]>limit){
+                hi--;
+                count++;
+            }
+            else if(people[lo]+people[hi]<=limit){
+                count++;
+                hi--;
+                lo++;
+            }
+        }
+        return count;
+    }
+};
