9d91ca78bbddff54b88ede9a9eacbba6922d6bb8
Muhammad Subhan <sbhnamir456@gmail.com>
2024-08-19 03:37:05 -0700

    Time: 55 ms (70.77%), Space: 45.7 MB (42.25%) - LeetHub

--- 0917-boats-to-save-people/0917-boats-to-save-people.cpp
+++ 0917-boats-to-save-people/0917-boats-to-save-people.cpp
@@ -4,15 +4,10 @@ public:
         int lo=0,hi=people.size()-1,count=0;
         sort(people.begin(),people.end());
         while(lo<=hi){
-            if(people[lo]+people[hi]>limit){
-                hi--;
-                count++;
-            }
-            else if(people[lo]+people[hi]<=limit){
-                count++;
-                hi--;
-                lo++;
-            }
+            if(people[lo]+people[hi]>limit)
+                hi--,count++;
+            else if(people[lo]+people[hi]<=limit)
+                count++,hi--,lo++;
         }
         return count;
     }
