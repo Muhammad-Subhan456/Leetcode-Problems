6664ed125a1e196514bd4be4d48299065908b3c7
Muhammad Subhan <sbhnamir456@gmail.com>
2024-08-22 09:27:41 -0700

    Time: 32 ms (25.57%), Space: 18.4 MB (48.69%) - LeetHub

---
+++ 0023-merge-k-sorted-lists/0023-merge-k-sorted-lists.cpp
@@ -0,0 +1,45 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+   ListNode* mergeTwoLists(ListNode* list1, ListNode* list2) {
+        ListNode* Dummy = new ListNode(-1); 
+        ListNode* ptr1 = list1;
+        ListNode* ptr2 = list2;
+        ListNode* ptr3 = Dummy;
+        while(ptr1 && ptr2){
+            if(ptr1->val < ptr2->val){
+                ptr3->next = ptr1;
+                ptr1 = ptr1->next;
+            }
+            else{
+                ptr3->next = ptr2;
+                ptr2=ptr2->next;
+            }
+            ptr3 = ptr3->next;
+        }
+         if(ptr1)
+            ptr3->next = ptr1;
+        else
+            ptr3->next=ptr2;
+        return Dummy->next;}
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        if(lists.size()==0)
+            return nullptr;
+        while(lists.size()>1){
+            ListNode* mergedHead = mergeTwoLists(lists[0],lists[1]);
+            lists.push_back(mergedHead);
+            lists.erase(lists.begin());
+            lists.erase(lists.begin());
+        }
+    return lists[0];
+    }
+};
