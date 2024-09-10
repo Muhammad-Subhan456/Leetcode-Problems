ca23d90bb05025b52002e813175177a9d89c4274
Muhammad Subhan <sbhnamir456@gmail.com>
2024-09-10 01:19:56 -0700

    Time: 13 ms (33.67%), Space: 7.1 MB (99.18%) - LeetHub

--- 1013-fibonacci-number/1013-fibonacci-number.cpp
+++ 1013-fibonacci-number/1013-fibonacci-number.cpp
@@ -2,8 +2,7 @@ class Solution {
 public:
     int fib(int n) {
     //Base Case
-    if(n==1) return 1;
-    if(n==0) return 0;
+    if(n==1 || n==0) return n; 
     return fib(n-1)+fib(n-2);
     }
 };      
