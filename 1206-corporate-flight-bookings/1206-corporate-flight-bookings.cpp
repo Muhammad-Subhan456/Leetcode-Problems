162d2708996da7e4577896c3bf8488841c107896
Muhammad Subhan <sbhnamir456@gmail.com>
2024-09-18 09:54:01 -0700

    Time: 135 ms (93.28%), Space: 71.5 MB (58.3%) - LeetHub

--- 1206-corporate-flight-bookings/1206-corporate-flight-bookings.cpp
+++ 1206-corporate-flight-bookings/1206-corporate-flight-bookings.cpp
@@ -1,13 +1,15 @@
 class Solution {
 public:
     vector<int> corpFlightBookings(vector<vector<int>>& bookings, int n) {
-    vector<int>freq(n+1);
-    for(int i=0; i < bookings.size();++i){
-        for(int j =bookings[i][0]; j<=bookings[i][1];++j){
-            freq[j]+= bookings[i][2];
-        }
+    vector<int>freq(n+2,0);
+    for(int i=0; i < bookings.size(); ++i){
+        freq[bookings[i][0]] += bookings[i][2];
+        freq[(bookings[i][1]+1)] -= bookings[i][2];
     }
+    for(int i=1; i<freq.size();++i)
+        freq[i]+=freq[i-1];
     freq.erase(freq.begin());
+    freq.pop_back();
     return freq;
     }
 };
