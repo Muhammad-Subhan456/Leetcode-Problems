6cf12fb1056cefa8e70204392be1107fe93d4db0
Muhammad Subhan <sbhnamir456@gmail.com>
2025-03-10 10:10:14 +0500

    Time: 0 ms (100%), Space: 8.4 MB (81.09%) - LeetHub

--- 3447-clear-digits/3447-clear-digits.cpp
+++ 3447-clear-digits/3447-clear-digits.cpp
@@ -1,24 +1,20 @@
 class Solution {
 public:
     string clearDigits(string s) {
-        stack<char>store;
         string ans;
-        for(int i=0;i<s.size();++i){
-            if(s[i]>=48 && s[i]<= 57)
-                store.pop();
-            else    
-                store.push(s[i]);
-        }
-        if(store.empty())
-            return "";
-        else{
-            while(!store.empty()){
-                ans.push_back(store.top());
-                store.pop();
-            }
-        }
-        reverse(ans.begin(),ans.end());
-        return ans;
+        for (char ch: s){
+            if(isdigit(ch)){
+                if (!ans.empty()){
+                    ans.pop_back();
+                }
+                else{
+                    ans.push_back(ch);
+                }
+            }else{
+                    ans.push_back(ch);
+                }
 
+        }
+        return ans;
     }
 };
