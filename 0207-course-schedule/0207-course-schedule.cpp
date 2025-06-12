bfb765b4c6b61af1bfeab0c8107f82370b462717
Muhammad Subhan <sbhnamir456@gmail.com>
2025-06-12 22:43:33 +0500

    Time: 8 ms (39.3%), Space: 21.9 MB (7.94%) - LeetHub

---
+++ 0207-course-schedule/0207-course-schedule.cpp
@@ -0,0 +1,52 @@
+class Solution {
+public:
+    vector<int> topoSort(int V,int E,vector<vector<int>> &edges)
+{
+    // create adj list
+    unordered_map<int,list<int>> adj;
+    for(int i=0; i < E; ++i){
+        int u = edges[i][0];
+        int v = edges[i][1];
+        adj[u].push_back(v);
+    }
+    
+    //find all the indegrees
+    vector<int>indegree(V);
+    for(auto i : adj){
+        for(auto j : i.second){
+            indegree[j]++;
+        }
+    }
+    
+    // push all nodes with 0 indegree
+    queue<int>q;
+    for(int i=0; i < V; ++i){
+        if(indegree[i]==0) q.push(i);
+    }
+
+    // Do Bfs
+    vector<int> ans;
+    while(!q.empty()){
+        int front = q.front();
+        q.pop();
+        ans.push_back(front);
+
+        // update indegree of neighbours
+
+        for(auto neighbour : adj[front]){
+            // reduce the indegrees by 1
+            indegree[neighbour]--;
+            // if indegree become zero push it in the queue
+            if(indegree[neighbour]==0) q.push(neighbour);
+        }
+    }
+
+    return ans;
+}  
+    bool canFinish(int v, vector<vector<int>>& edges) {
+        int E = edges.size();
+        vector<int>ans = topoSort(v,E,edges);
+        //for(auto c : ans) cout << c << " ";
+        return v == ans.size() ? true : false;     
+    }
+};
