92256d55ff589466bd3cd499a78b7ec4a769e618
Muhammad Subhan <sbhnamir456@gmail.com>
2025-07-06 15:11:42 +0500

    Time: 599 ms (62.43%), Space: 368.5 MB (26.32%) - LeetHub

--- 3927-minimum-cost-path-with-alternating-directions-ii/3927-minimum-cost-path-with-alternating-directions-ii.cpp
+++ 3927-minimum-cost-path-with-alternating-directions-ii/3927-minimum-cost-path-with-alternating-directions-ii.cpp
@@ -1,23 +1,25 @@
 class Solution {
 public:
     using ll = long long;
-    long long rec(ll i,ll j,ll s,vector<vector<int>>&wc,vector<vector<vector<ll>>>&dp){
+    long long rec(long long i,long long j,long long s,vector<vector<int>>&wc,
+    vector<vector<vector<ll>>>&dp){
+        if(i < 0 || j < 0 ) return 1e18;
         if(i==0 && j==0) return 0;
-        if(i < 0 || j < 0 ) return 1e15;
         if(dp[i][j][s] != -1) return dp[i][j][s];
-        long long ans = LLONG_MAX;
+        long long up = LLONG_MAX, left = LLONG_MAX, ans=0;
         if(s&1){
-        ans = min(ans,(i+1)*(j+1)+rec(i-1,j,0,wc,dp));
-        ans = min(ans,(i+1)*(j+1)+rec(i,j-1,0,wc,dp));
-
+         up =  ((i+1)*(j+1)) + rec(i-1,j,0,wc,dp);
+        left =  ((i+1)*(j+1)) + rec(i,j-1,0,wc,dp);
+        ans = min(up,left);
         }
         else
-        ans = min(ans,wc[i][j] + rec(i,j,1,wc,dp));
+        ans = (wc[i][j]+rec(i,j,1,wc,dp));
         return dp[i][j][s] = ans;
-
     }
+
+        
     long long minCost(int m, int n, vector<vector<int>>& waitCost) {
-   vector<vector<vector<ll>>> dp(m, vector<vector<ll>> (n, vector<ll> (2, -1)));
-        return 1LL+rec(m-1,n-1,1,waitCost,dp);
+    vector<vector<vector<ll>>> dp(m, vector<vector<ll>> (n, vector<ll> (2, -1)));
+        return 1+rec(m-1,n-1,1,waitCost,dp);
     }
 };
