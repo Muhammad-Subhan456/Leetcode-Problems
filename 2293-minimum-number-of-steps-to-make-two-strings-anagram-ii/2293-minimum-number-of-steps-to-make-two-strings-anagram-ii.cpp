c532de48035d6367504767024ac1f01ac987f7f7
Muhammad Subhan <sbhnamir456@gmail.com>
2025-08-04 14:44:45 +0500

    Time: 116 ms (8.84%), Space: 32.3 MB (7.71%) - LeetHub

---
+++ 2293-minimum-number-of-steps-to-make-two-strings-anagram-ii/2293-minimum-number-of-steps-to-make-two-strings-anagram-ii.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    int minSteps(string s, string t) {
+        map<char,int>mp1;
+        map<char,int>mp2;
+        int sum=0;
+        for(auto c : s) mp1[c]++;
+        for(auto c : t) mp2[c]++;
+        // for(auto p : mp1){
+        //     cout << p.first << "->" << p.second << endl;
+        // }
+        // cout << "mp2" << endl;
+        // for(auto p : mp2){
+        //     cout << p.first << "->" << p.second << endl;
+        // }
+        for(auto &p : mp2){
+            int mini = INT_MAX;
+            if(mp1[p.first]){
+                mini = min(p.second,mp1[p.first]);
+                p.second -= mini;
+                mp1[p.first] -= mini;
+            }
+        }
+        for(auto p : mp1){
+            sum += p.second;
+        }
+        for(auto p : mp2){
+            sum += p.second;
+        }
+        
+        return sum;
+    }
+};
