5b5539b3c0bd2a3115d7c449357ee41ae2aaf2e3
Muhammad Subhan <sbhnamir456@gmail.com>
2025-12-20 11:42:03 +0500

    Time: 3 ms (71.47%), Space: 8.3 MB (88.92%) - LeetHub

---
+++ 3461-check-if-digits-are-equal-in-string-after-operations-i/3461-check-if-digits-are-equal-in-string-after-operations-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasSameDigits(string s) {
+        while (s.size()>2)
+    {
+        for(int i=1; i < s.size(); ++i) 
+            s[i-1] = ((s[i]-'0')+ (s[i-1]-'0'))%10;
+        
+        s.pop_back();
+    }
+    return (s[0] == s[1] ? true : false);
+        
+    }
+};
