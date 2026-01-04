622d741862f5c5dc57ce6b5f23e23490f3a8f3ef
Muhammad Subhan <sbhnamir456@gmail.com>
2026-01-04 20:15:29 +0500

    Time: 47 ms (31.62%), Space: 49 MB (30.56%) - LeetHub

--- 0322-coin-change/0322-coin-change.cpp
+++ 0322-coin-change/0322-coin-change.cpp
@@ -1,25 +1,22 @@
 class Solution {
 public:
-    using vii = vector<vector<int>>;
-    using vi = vector<int>;
-    int coinChange(vector<int>& a, int k) {
-        int n = a.size();
-        vii dp(n, vi(k + 1,-1));
-    function<int(int, int)> rec = [&](int i, int k) -> int
-    {
-        if(i == 0){
-            if(k % a[i] == 0) return k/a[i];
-            return 1e9;
+    int coinChange(vector<int>& coins, int amount) {
+        int n = coins.size();
+        vector<vector<int>>dp(n,vector<int>(amount+1,0));
+        for(int t=0; t <= amount ; ++t){
+            if(t % coins[0] == 0) dp[0][t] = t/coins[0] ;
+            else dp[0][t] = 1e9;
         }
-        if(dp[i][k] != -1) return dp[i][k];
-        int nt = 0 + rec(i-1,k);
-        int t = 1e9;
-        if(a[i] <= k){
-            t = 1 + rec(i,k-a[i]);
+        for(int i=1; i < n; ++i){
+            for(int t=0; t <= amount; ++t){
+                int nt = dp[i-1][t];
+                int take = 1e9;
+                if(coins[i] <= t){
+                    take = 1 + dp[i][t-coins[i]];
+                }
+                dp[i][t] = min(take,nt);
+            }
         }
-        return dp[i][k] = min(t,nt);
-    };
-    int ans = rec(n-1,k);
-    return ans < 1e9 ?  ans :  -1;
+        return dp[n-1][amount] < 1e9 ? dp[n-1][amount] : -1;
     }
 };
