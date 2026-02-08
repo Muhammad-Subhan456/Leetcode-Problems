26ca5317a08dc1673a8017a8c0f09272b61e3523
Muhammad Subhan <sbhnamir456@gmail.com>
2026-02-08 12:23:48 +0500

    Time: 64 ms (5.1%), Space: 23.1 MB (20.68%) - LeetHub

--- 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
+++ 0110-balanced-binary-tree/0110-balanced-binary-tree.cpp
@@ -11,26 +11,16 @@
  */
 class Solution {
 public:
-pair<bool,int>isBalancedFast(TreeNode* root){
-    if(root==NULL){
-        pair<bool,int> p = make_pair(true,0);
-        return p;
+    int height(TreeNode* root){
+        if(!root) return 0;
+        return max(height(root->left),height(root->right))+1;
     }
-    pair<int,int> left = isBalancedFast(root->left);
-    pair<int,int> right = isBalancedFast(root->right);
-    bool leftans = left.first;
-    bool rightans = right.first;
-    bool diff = abs(left.second-right.second) <=1;
-    pair<bool,int> ans;
-    ans.second = max(left.second,right.second)+1;
-    if(leftans && rightans && diff){
-        ans.first = true;
-    }
-    else ans.first = false;
-    return ans;
-}
-
     bool isBalanced(TreeNode* root) {
-    return isBalancedFast(root).first;
+    if(root==NULL) return true;
+    bool left = isBalanced(root->left);
+    bool right = isBalanced(root->right);
+    bool diff = abs(height(root->left)-height(root->right))<=1;
+    if(left && right && diff) return true;
+    return false;
     }
 };
