51f0e601ee9dabd5d4ef61ca1334923fc83a674a
Muhammad Subhan <sbhnamir456@gmail.com>
2026-02-10 13:39:00 +0500

    Time: 7 ms (55.06%), Space: 25.7 MB (41.64%) - LeetHub

--- 3379-transformed-array/3379-transformed-array.cpp
+++ 3379-transformed-array/3379-transformed-array.cpp
@@ -4,20 +4,8 @@ public:
         int n = nums.size(),temp=0;
         vector<int>o(n);
         for(int i=0; i < n; ++i){
-            if(nums[i] == 0) o[i] = nums[i];
-            if(nums[i] > 0){
-                temp = nums[i]%n;
-                o[i] =  nums[(i+temp)%n];
-            }
-            else{
-                temp = abs(nums[i]);
-                temp = temp%n;
-               // cout << temp << '\n';
-                temp = i - temp;
-                if(temp >= 0) o[i] = nums[temp];
-                else
-                o[i] =  nums[n+temp];
-            }
+            int idx = (i + nums[i] % n + n) % n;
+            o[i] = nums[idx];
         }
         return o;       
     }
