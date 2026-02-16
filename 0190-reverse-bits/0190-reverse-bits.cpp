2a51f34a4dbc7bfd56e1832b645a3b90c9faa8a5
Muhammad Subhan <sbhnamir456@gmail.com>
2026-02-16 12:09:36 +0500

    Time: 0 ms (100%), Space: 8.3 MB (15.72%) - LeetHub

--- 0190-reverse-bits/0190-reverse-bits.cpp
+++ 0190-reverse-bits/0190-reverse-bits.cpp
@@ -4,7 +4,7 @@ public:
     ll dec(string s){
         ll sum=0;
         for(ll i=0; i <= 31;++i){
-            sum += (1LL*(s[i]-'0')*(1<<i));
+            sum += (1LL*(s[i]-'0')*(1LL<<i));
         }
         return sum;
     }
