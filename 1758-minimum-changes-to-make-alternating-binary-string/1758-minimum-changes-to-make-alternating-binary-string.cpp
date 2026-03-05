194337c556139de44352b9c95b860186808846c9
Muhammad Subhan <sbhnamir456@gmail.com>
2026-03-05 15:14:50 +0500

    Time: 4 ms (4.15%), Space: 9.6 MB (14.85%) - LeetHub

--- 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
+++ 1758-minimum-changes-to-make-alternating-binary-string/1758-minimum-changes-to-make-alternating-binary-string.cpp
@@ -15,7 +15,6 @@ int miniii(string &s,int &cnt){
     int minOperations(string s) {
         string s1 = s;
         string s2 = s;
-        string s3 = s;
         int cnt = 0;
         // pass 1 
        int min1 = miniii(s1,cnt);
@@ -30,24 +29,6 @@ int miniii(string &s,int &cnt){
             s2[0] = '0';
             min1 = min(min1,miniii(s2,cnt));
         }
-        // pass 3
-        reverse(s3.begin(),s3.end());
-        string s4 = s3;
-        cnt = 0;
-    
-        int min2 = miniii(s3,cnt);
-        // pass 4
-        if(s4[0] == '0'){
-            cnt = 1;
-            s4[0] = '1';
-            min2 = min(min2,miniii(s4,cnt));
-        }
-        else{
-            cnt = 1;
-            s4[0] = '0';
-            min2 = min(min2,miniii(s4,cnt));
-        }
-        cout << min2;
-        return min(min1,min2);
+        return min1;
     }
 };
