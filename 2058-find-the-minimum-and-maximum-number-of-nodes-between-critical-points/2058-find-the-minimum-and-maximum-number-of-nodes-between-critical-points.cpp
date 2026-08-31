ff4b69a7263563535000539f5d08cc81922dad97
Muhammad Subhan <sbhnamir456@gmail.com>
2026-08-31 15:47:43 +0500

    Time: 9 ms (17.24%), Space: 124.7 MB (44.1%) - LeetHub

--- 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
+++ 2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points/2058-find-the-minimum-and-maximum-number-of-nodes-between-critical-points.cpp
@@ -1,66 +1,50 @@
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode() : val(0), next(nullptr) {}
- *     ListNode(int x) : val(x), next(nullptr) {}
- *     ListNode(int x, ListNode *next) : val(x), next(next) {}
- * };
- */
 class Solution {
 public:
     vector<int> nodesBetweenCriticalPoints(ListNode* head) {
-        int len = 0;
-        ListNode* temp = head;
-        while(temp){
-            temp = temp->next;
-            len++;
-        }
-        if(len <= 2) return {-1,-1};
+        int first = -1;
+        int prev = -1;
 
-        int i = 2;
-        ListNode* prev = head;
-        temp = head->next;
-        ListNode* nex = temp->next;
-        int mini = INT_MAX, maxi = INT_MIN;
-        int sec = INT_MAX;
-        int a = -1, b = -1;
-        bool flag = true;
-        while(nex){
-            int x = prev->val, y = temp->val, z = nex->val;
-            if((y > x && y > z) || (y < x && y < z)  ){
-                // maximum
-                mini = min(mini,i);
-                maxi = max(maxi,i);
-                // minimum
-                if(flag){
-                    flag = false;
-                    a = i;
-                }
-                else {
-                    flag = true;
-                    b = i;
-                }
-                if( a!= -1 && b != -1) {
-                    sec = min(abs(b-a),sec);
+        int minDist = INT_MAX;
+        int maxDist = -1;
+
+        ListNode* left = head;
+        ListNode* mid = head->next;
+
+        int pos = 1;
+
+        while (mid && mid->next) {
+            ListNode* right = mid->next;
+
+            // Check if mid is a critical point
+            if ((mid->val > left->val && mid->val > right->val) ||
+                (mid->val < left->val && mid->val < right->val)) {
+
+                if (first == -1) {
+                    // First critical point
+                    first = pos;
                 }
 
+                if (prev != -1) {
+                    // Distance from previous critical point
+                    minDist = min(minDist, pos - prev);
+                }
 
+                prev = pos;
+
+                // Distance from first to current (last) critical point
+                maxDist = pos - first;
             }
-            i++;
-            prev = prev->next;
-            temp = temp->next;
-            nex = nex->next;
-            
 
+            left = mid;
+            mid = right;
+            pos++;
         }
-        vector<int>res;
-        if(a == -1 || b == -1) return {-1,-1};
-        else res.push_back(sec);
-        if(mini == INT_MAX || maxi == INT_MIN) return {-1,-1};
-        else res.push_back(maxi-mini);
-        return res;
-        
+
+        // Need at least two critical points
+        if (first == -1 || first == prev) {
+            return {-1, -1};
+        }
+
+        return {minDist, maxDist};
     }
 };
